8f2672bfeed52db65fdb9f096ae633515345f706
monu1315 <72178131+monu1315@users.noreply.github.com>
2022-06-22 15:37:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,10 @@ var secondItem = document.querySelector('.list-group-item:nth-child(1)');
 secondItem.style.backgroundColor = 'green';
 var thirdItem = document.querySelector('.list-group-item:nth-child(2)');
 thirdItem.style.visibility = 'hidden';
+var secondItem = document.querySelectorAll('li:nth-child(1)');
+secondItem[1].style.color = 'green';
+var odd  = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor = 'green';
+}
