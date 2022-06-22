7d31938c6e1655ca46fdf0280508228c5a1f0808
monu1315 <72178131+monu1315@users.noreply.github.com>
2022-06-22 17:27:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,3 +34,42 @@ for(var i=0;i<odd.length;i++)
 }
 var ss = document.querySelectorAll('li');
 ss[1].style.color  = 'green';
+var itemList = document.querySelector('#items');
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+
+// parent Element is same thing
+var itemList = document.querySelector('#items');
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// //Child items ;;
+// for all childs 
+itemList.children.style.backgroundColor = '#f4f4f4';
+//first child ;;
+itemList.firstChild.style.backgroundColor = 'yellow';
+//firstElement child ;; 
+itemList.firstElementChild.textContent = 'Hello';
+//last chlast;
+itemList.lastChild.style.backgroundColor = 'yellow';
+//lastElement;; 
+itemList.lastElementChild.textContent = 'Hello';
+//nextsibling ;;
+itemList.nextSibling.textContent =  'Monu';
+//nextElement sibling ;;
+itemList.nextElementSibling.style.backgroundColor = 'black';
+//previoussibling ;;
+itemListList.previousElementSibling.style.color = 'yellow';
+//create element ;;
+var newDiv = document.createElement('div');
+// add classs 
+newDiv.className = 'hello';
+//add id ;
+newDiv.id = 'hello1';
+//add atr.
+newDiv.setAttribute('title','Hello Div');
+//creating text node ;;
+var newDivText = document.createTextNode('Hello World');
+// add text to div ;
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header .container');
+var h1 = documetn.querySelector(' header h1');
+newDiv.style.fontSize = '30px';
+container.insertBefore(newDiv,h1);
