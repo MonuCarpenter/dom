192d6935b6324335924344b3a029f0a21f4e82fe
monu1315 <72178131+monu1315@users.noreply.github.com>
2022-06-22 11:18:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var tt = document.querySelector('h2');
 tt.style.font = 'solid 3px ###2';
 tt.style.color = "green";
+var items = document.getElementsByClassName('list-group-item');
+items[3].style.backgroundColor = 'green';
+items[0].textContent='MONU';
+
+items[1].textContent='VINOD';
+
+items[2].textContent='TADAS';
+
+items[3].textContent='WARDHA';
+
+
