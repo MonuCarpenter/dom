3462c6e70f89951c232328f4e374e3a1534f7ff5
monu1315 <72178131+monu1315@users.noreply.github.com>
2022-06-22 15:51:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,10 @@ for(var i=0;i<odd.length;i++)
 {
     odd[i].style.backgroundColor = 'green';
 }
+var odd  = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor = 'green';
+}
+var ss = document.querySelectorAll('li');
+ss[1].style.color  = 'green';
